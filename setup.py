f80b937035d8e1a6e7e878796980b2b79c4d0fc0
bvandercar-vt <bvandercar@outlook.com>
2025-05-06 10:08:48 -0600

    fix folder structure tree node stuff

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='mixed_in_key_automator',
-   version='1.0.1',
+   version='1.2.0',
    description='Windows automator for Mixed In Key. Known works for MIK versions 10 and 11',
    author='Blake Vandercar',
    author_email='TODO',
--- src/mik_automator.py
+++ src/mik_automator.py
@@ -3,8 +3,13 @@ import logging
 import os
 import time
 from pathlib import Path
+import ctypes
 
-from pywinauto import application
+from pywinauto.application import Application, WindowSpecification
+from pywinauto.controls.uia_controls import ButtonWrapper, TreeItemWrapper
+from pywinauto.controls.common_controls import TreeViewWrapper
+from pywinauto.base_wrapper import BaseWrapper
+
 
 root_dir = os.getenv("LOCALAPPDATA")
 assert root_dir, "local appdata"
@@ -18,9 +23,30 @@ assert len(exe_files) == 1, "more than 1 mik .exe found"
 exe_file = os.path.join(root_dir, exe_files[0])
 
 
+def split_folder(folder: str):
+    return [i for i in folder.split(os.sep) if i]
+
+
+def get_screen_resolution() -> tuple[int, int]:
+    user32 = ctypes.windll.user32
+    return user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+
+
+resolution = get_screen_resolution()
+
+
+def scale_coords(coords: tuple[int, int], base_resolution=tuple[int, int]):
+    """
+    base_resolution: the resolution that we found the coords on
+    """
+    return int(coords[0] * resolution[0] / base_resolution[0]), int(
+        coords[1] * resolution[1] / base_resolution[1]
+    )
+
+
 # Create window (or connect to existing)
 def get_mik_window(create_new_if_not_open: bool):
-    app = application.Application(backend="uia")
+    app = Application(backend="uia")
 
     for i in range(3):
         try:
@@ -50,62 +76,80 @@ def run(folder: str):
         )
 
     window, app = get_mik_window(create_new_if_not_open=True)
-
+    window_wrapper: BaseWrapper = window
     # window.print_control_identifiers()
 
     # Maximize window to make coordinate clicking consistent.
     try:
-        window.child_window(auto_id="MaximizeButton").click()
+        maximize_btn: ButtonWrapper = window.child_window(auto_id="MaximizeButton")
+        maximize_btn.click()
     except:
         pass  # already maximized
 
     window.child_window(control_type="ProgressBar").wait_not("visible", timeout=60 * 5)
 
-    # Add Files button
-    window.click_input(coords=(111, 325))
+    # Add Files button-- cannot find an identifier, have to just click coords
+    window_wrapper.click_input(
+        coords=scale_coords(coords=(111, 325), base_resolution=(1920, 1080))
+    )
     # Add Folder button in dialog
-    window.child_window(best_match="ADD FOLDERButton").click()
+    add_folder_btn: ButtonWrapper = window.child_window(best_match="ADD FOLDERButton")
+    add_folder_btn.click()
 
     # Folder tree
-    node = window.Pane.TreeView.get_item(["Desktop", "This PC"], False)
-    node.ensure_visible()
-    node.click_input()
-    time.sleep(1)
-    subfolder = folder.split(f"{USER_FOLDER}\\", 1)[1]
-    subfolders = subfolder.split("\\")
-    for f in subfolders[:-1]:
-        node = node.get_child(f)
+    tree: TreeViewWrapper = window.Pane.TreeView
+
+    def expand_nodes(originally_visible_nodes: list[str], remaining_nodes: list[str]):
+        node: TreeItemWrapper = tree.get_item(originally_visible_nodes, False)
         node.ensure_visible()
         node.click_input()
         time.sleep(1)
-    node = node.get_child(subfolders[-1])
-    node.select()
-    time.sleep(2)
+        for node_str in remaining_nodes[:-1]:
+            node = node.get_child(node_str)
+            node.ensure_visible()
+            node.click_input()
+            time.sleep(1)
+        node = node.get_child(remaining_nodes[-1])
+        node.select()
+        time.sleep(2)
+
+    is_in_user_folder = False
+    for user_folder in ["Music", "Downloads", "Documents"]:
+        user_folder_long = f"{USER_FOLDER}\\{user_folder}"
+        if user_folder_long in folder:
+            is_in_user_folder = True
+            expand_nodes(
+                originally_visible_nodes=["Desktop", user_folder],
+                remaining_nodes=split_folder(folder.split(user_folder_long, 1)[1]),
+            )
+            break
+    if not is_in_user_folder:
+        expand_nodes(
+            originally_visible_nodes=["Desktop", "This PC"],
+            remaining_nodes=split_folder(os.path.abspath(folder)),
+        )
 
     # OK after selected folder
     folder_browser_popup = window.child_window(best_match="Browse For Folder")
-    ok_button = folder_browser_popup.child_window(best_match="OK Button")
-    ok_button.click_input()
+    ok_btn: ButtonWrapper = folder_browser_popup.child_window(best_match="OK Button")
+    ok_btn.click_input()
 
     # Wait for files to be added
-    window.child_window(best_match="Adding files to queue").wait(
-        "visible", timeout=60 * 5
-    )
-    window.child_window(best_match="Adding files to queue").wait_not(
-        "visible", timeout=60 * 5
-    )
+    ADDING_FILES = "Adding files to queue"
+    window.child_window(best_match=ADDING_FILES).wait("visible", timeout=60 * 5)
+    window.child_window(best_match=ADDING_FILES).wait_not("visible", timeout=60 * 5)
     window.child_window(control_type="ProgressBar").wait_not("visible", timeout=60 * 5)
 
     # Attempt to close. If window closes, analysis done. If confirmation popup appears, is not done.
     while True:
-        window.child_window(auto_id="CloseButton").click()
+        close_button: ButtonWrapper = window.child_window(auto_id="CloseButton")
+        close_button.click()
         try:
             # Window item gets all weird here, sometimes can't find the NO button. Re-connect to the window.
             del app
             window, app = get_mik_window(create_new_if_not_open=False)
-            no_button = window.child_window(best_match="NOButton").wait(
-                "visible", timeout=5
-            )
+            no_button: ButtonWrapper = window.child_window(best_match="NOButton")
+            no_button.wait("visible", timeout=5)
         except KeyboardInterrupt:
             input("press any key to continue checking mik")
             break
@@ -116,6 +160,6 @@ def run(folder: str):
 
 
 if __name__ == "__main__":
-    folder = os.path.join(Path.home(), "Music\\downloaded")
-    print("downloading from: ", folder)
+    folder = os.path.join(Path.home(), "Music\\DJ Tracks")
+    print("adding folder: ", folder)
     run(folder)
